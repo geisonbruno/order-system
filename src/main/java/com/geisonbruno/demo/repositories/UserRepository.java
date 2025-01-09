4b40cd44defaf9b825b62ca56d0e32eb3ec115e9
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-01-09 11:34:14 -0300

    feat: JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/geisonbruno/demo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.geisonbruno.demo.repositories;
+
+import com.geisonbruno.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
