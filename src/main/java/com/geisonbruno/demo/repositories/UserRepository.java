1a89babb8505cc811e3e433ad1121a8327220b41
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-01-10 11:11:38 -0300

    feat: Service layer, component registration

--- src/main/java/com/geisonbruno/demo/repositories/UserRepository.java
+++ src/main/java/com/geisonbruno/demo/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.geisonbruno.demo.repositories;
 
 import com.geisonbruno.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
