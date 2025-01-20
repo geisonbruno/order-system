79514b62f552db3245231cdc8cca916300c0b2a9
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-01-20 15:37:53 -0300

    feat: Exception handling - findById

--- src/main/java/com/geisonbruno/demo/services/UserService.java
+++ src/main/java/com/geisonbruno/demo/services/UserService.java
@@ -2,6 +2,7 @@ package com.geisonbruno.demo.services;
 
 import com.geisonbruno.demo.entities.User;
 import com.geisonbruno.demo.repositories.UserRepository;
+import com.geisonbruno.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
