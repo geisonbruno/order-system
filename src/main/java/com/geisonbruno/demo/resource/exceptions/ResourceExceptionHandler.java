ea22fe97008cdd6789d9a084b00be56261a9ae8a
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-01-20 16:31:37 -0300

    feat: Exception handling - delete

--- src/main/java/com/geisonbruno/demo/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/geisonbruno/demo/resource/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.geisonbruno.demo.resource.exceptions;
 
+import com.geisonbruno.demo.services.exceptions.DatabaseException;
 import com.geisonbruno.demo.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -20,4 +21,14 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
--- src/main/java/com/geisonbruno/demo/services/UserService.java
+++ src/main/java/com/geisonbruno/demo/services/UserService.java
@@ -2,8 +2,11 @@ package com.geisonbruno.demo.services;
 
 import com.geisonbruno.demo.entities.User;
 import com.geisonbruno.demo.repositories.UserRepository;
+import com.geisonbruno.demo.services.exceptions.DatabaseException;
 import com.geisonbruno.demo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }  catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
